356da0336ac9896c7c78d03dcdb21c30ec3eb12e
Andrey Belyaev <belyaev.andrey@gmail.com>
2021-12-06 12:18:12 +0400

    Added javadoc

--- data-graphql/src/main/java/org/haulmont/rnd/data/config/GraphQlQuery.java
+++ data-graphql/src/main/java/org/haulmont/rnd/data/config/GraphQlQuery.java
@@ -2,6 +2,9 @@ package org.haulmont.rnd.data.config;
 
 import java.lang.annotation.*;
 
+/**
+ * EXPERIMENTAL, need to be able to define object name here.
+ */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface GraphQlQuery {
